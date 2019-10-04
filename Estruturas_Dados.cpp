d5fb57bc2a647b7ff0f789edb949e4640c1c4626
VagnerSharp <55877172+VagnerSharp@users.noreply.github.com>
2019-10-04 16:00:58 -0300

    pilha em C/C++
    usando o arquivo Header (pilha.h), se tem a estrutura de dados no arquivo cpp (os códigos estão misturados entre C e C++, mostrando uma integração das linguagens ATENÇÃO: não é recomendável a mistura de códigos C com C++, somente se houver um propósito)

---
+++ Estruturas_Dados.cpp
@@ -0,0 +1,101 @@
+//Estrutura_Dados.cpp 
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <stack>
+#include <Windows.h>
+#include <locale.h>
+#include "pilha.h"
+
+using namespace std;
+
+int main()
+{
+
+	system("color 75");
+	SetConsoleTitleA("ESTRUTURA DE DADOS - PILHA EM C E C PLUS PLUS");
+	
+	
+	setlocale(LC_ALL, "Portuguese");
+
+	printf("Hello World \n");
+
+	int quant;
+
+	//Ponteiro para a pilha, o tipo T agora é String
+	pilha<string>*stacks = new pilha<string>;
+
+	//Inicia a pilha
+	stacks->stackOpen();
+
+	printf("Deseja adicionar quantos elementos na pilha ? ");
+	scanf_s(" %d", &quant);
+	printf("\n");
+
+	cin.ignore();
+
+	//Quantidade não pode chegar ao limite da pilha
+	if (quant < 999)
+	{
+		int insert,query;
+		string in, querys;
+		
+
+		//Adiciona elementos na pilha até a quantidade especificada
+		for (int i = 0; i < quant; i++)
+		{
+			system("cls");
+			printf("%d° elemento: ", (i + 1));
+			getline(cin, in);
+		
+
+			//Adiciona o elemento digitado ao topo da pilha
+			stacks->push(in);
+		}
+
+		printf("\nAguarde... ");
+		Sleep(3000);
+		system("cls");
+
+		printf("Elementos da pilha: \n\n");
+
+		//Mostra todos os elementos da pilha, removendo quem esta no topo
+		for (int i = 0; i < quant; i++)
+		{
+			//O endereço da variavel é adiconada ao ponteiro da função 'pop'
+			stacks->pop(&querys);
+
+			//A varivel agora tem acesso aos elementos
+			cout<<(i+1)<<"°elemento: "<<querys<<endl;
+		}
+		printf("\n\n");
+		int a;
+		printf("Remova: ");
+		scanf_s(" %d", &a);
+		stacks->remove(a);
+		querys = "";
+		for (int i = 0; i < quant; i++)
+		{
+			//O endereço da variavel é adiconada ao ponteiro da função 'pop'
+			stacks->pop(&querys);
+
+			//A varivel agora tem acesso aos elementos
+			cout << (i + 1) << "°elemento: " << querys << endl;
+		}
+	}
+	else
+	{
+		printf("Valor muito grande para a pilha !\n");
+	}
+
+    //Libera o espaço de memória usado pelo ponteiro (alocação dinâmica de memória, em C é o free)
+	delete stacks;
+	
+
+	printf("\n\n");
+	system("pause");
+
+	return 0;
+}
